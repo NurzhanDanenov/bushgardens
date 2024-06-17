45d7a472c344bea23b51a1b80848ebe006e4bf4d
NurzhanDanenov <132642544+NurzhanDanenov@users.noreply.github.com>
2024-06-17 14:48:17 +0500

    Add files via upload

--- bushgardens/config/firebase.go
+++ bushgardens/config/firebase.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/db"
+	"firebase.google.com/go/v4/storage"
 	"fmt"
 	"google.golang.org/api/option"
 	"os"
@@ -13,9 +14,13 @@ type FireDB struct {
 	*db.Client
 }
 
+type FireStorage struct {
+	*storage.Client
+}
+
 var fireDB FireDB
 
-func NewFireDB() (*FireDB, error) {
+func InitializeFirebaseApp() (*firebase.App, error) {
 	// Find home directory.
 	home, err := os.Getwd()
 	if err != nil {
@@ -28,53 +33,38 @@ func NewFireDB() (*FireDB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error initializing app: %v", err)
 	}
+	//client, err := app.Database(ctx)
+	//if err != nil {
+	//	return nil, fmt.Errorf("error initializing database: %v", err)
+	//}
+	//return &FireDB{Client: client}, nil
+	return app, nil
+}
+
+func NewFireDB() (*FireDB, error) {
+	// Initialize Firebase Realtime Database client.
+	ctx := context.Background()
+	app, err := InitializeFirebaseApp()
 	client, err := app.Database(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("error initializing database: %v", err)
+		return nil, fmt.Errorf("error initializing database client: %v", err)
 	}
+
 	return &FireDB{Client: client}, nil
 }
 
-//func NewFireDB() (*FireDB, error) {
-//	// Find home directory.
-//	home, err := os.Getwd()
-//	if err != nil {
-//		return nil, err
-//	}
-//	ctx := context.Background()
-//	opt := option.WithCredentialsFile(home + "\\main\\bushgardens-e7339-firebase-adminsdk-42jdi-c6d0b389d6.json")
-//	config := &firebase.Config{DatabaseURL: "https://bushgardens-e7339-default-rtdb.asia-southeast1.firebasedatabase.app/"}
-//	app, err := firebase.NewApp(ctx, config, opt)
-//	if err != nil {
-//		return nil, fmt.Errorf("error initializing app: %v", err)
-//	}
-//	client, err := app.Database(ctx)
-//	if err != nil {
-//		return nil, fmt.Errorf("error initializing database: %v", err)
-//	}
-//	return &FireDB{Client: client}, nil
-//}
+// NewFireStorage initializes the FireStorage client and returns it.
+func NewFireStorage() (*FireStorage, error) {
+	// Initialize Firebase Storage client.
+	ctx := context.Background()
+	app, err := InitializeFirebaseApp()
+	client, err := app.Storage(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing storage client: %v", err)
+	}
 
-//func (db *FireDB) Connect() error {
-//	// Find home directory.
-//	home, err := os.Getwd()
-//	if err != nil {
-//		return err
-//	}
-//	ctx := context.Background()
-//	opt := option.WithCredentialsFile(home + "\\main\\bushgardens-e7339-firebase-adminsdk-42jdi-c6d0b389d6.json")
-//	config := &firebase.Config{DatabaseURL: "https://bushgardens-e7339-default-rtdb.asia-southeast1.firebasedatabase.app/"}
-//	app, err := firebase.NewApp(ctx, config, opt)
-//	if err != nil {
-//		return fmt.Errorf("error initializing app: %v", err)
-//	}
-//	client, err := app.Database(ctx)
-//	if err != nil {
-//		return fmt.Errorf("error initializing database: %v", err)
-//	}
-//	db.Client = client
-//	return nil
-//}
+	return &FireStorage{Client: client}, nil
+}
 
 func FirebaseDB() *FireDB {
 	return &fireDB
